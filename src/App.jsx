ff60a6e476f33a1f20d6c1ff21ab9a7e5a9406fa
Alexis Luna <mceclipse1889@gmail.com>
2025-11-15 20:01:15 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import PixelPerfect from "./components/PixelPerfect.jsx";
-import Animation from "./components/Animation.jsx"
+import Animation from "./components/Animation.jsx";
+import Priority from "./components/Priority.jsx";
 
-function App() {
+export default function App() {
     return (
         <main>
             <Header/>
             <Hero/>
             <PixelPerfect/>
             <Animation/>
+            <Priority/>
         </main>
     );
 }
-
-export default App;
