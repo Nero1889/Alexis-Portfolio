47ed04b1eef8348fd3ac4e06200ce7e2c3da3a07
Alexis Luna <mceclipse1889@gmail.com>
2025-11-17 16:45:17 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React, {useState, useRef, useEffect} from "react";
+import {gsap} from "gsap";
 import menu from "/icons/menu.png";
 
 export default function Priority() {
     const [isOpen, setIsOpen] = useState(false);
     const menuRef = useRef(null);
+    const navRef = useRef(null);
+    const tl = useRef(null);
 
-    const toggleMenu = () => setIsOpen(!isOpen);
+    const toggleMenu = () => setIsOpen((prev) => !prev);
 
     useEffect(() => {
         function handleClickOutside(e) {
@@ -13,28 +16,46 @@ export default function Priority() {
                 setIsOpen(false);
             }
         }
-        
         document.addEventListener("mousedown", handleClickOutside);
         return () => document.removeEventListener("mousedown", handleClickOutside);
-        
-    }, [menuRef]);
+    }, []);
+
+    useEffect(() => {
+        tl.current = gsap.timeline({paused: true})
+            .fromTo(
+                navRef.current,
+                { 
+                    opacity: 0,
+                    scale: 0.95,
+                    transformOrigin: "top right",
+                    display: "none",
+                }, {
+                    opacity: 1,
+                    scale: 1,
+                    display: "block",
+                    duration: 0.1,
+                    ease: "power3.out",
+                }
+            );
+    }, []);
+
+    useEffect(() => {
+        isOpen ? tl.current.play() : tl.current.reverse();
+    }, [isOpen]);
 
     return (
         <header>
-            <div ref={menuRef}> 
-                <img src={menu} alt="Menu" draggable="false"
-                onClick={toggleMenu}/>
-                        
-                {isOpen && (
-                    <nav>
-                        <ul>
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#work">Work</a></li>
-                            <li><a href="#contact">Contact</a></li>
-                        </ul>
-                    </nav>
-                )}
+            <div ref={menuRef}>
+                <img src={menu} alt="Menu" draggable="false" onClick={toggleMenu}/>
+
+                <nav ref={navRef}>
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#work">Work</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
