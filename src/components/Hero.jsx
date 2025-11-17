de6ff1e90e3297134269430ba34949de36106629
Alexis Luna <mceclipse1889@gmail.com>
2025-11-17 16:45:35 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,44 @@
-function Hero() {
+import React, {useEffect, useRef} from "react";
+import {gsap} from "gsap";
+
+export default function Hero() {
+    const titleRef = useRef(null);
+
+    useEffect(() => {
+        const spans = titleRef.current.querySelectorAll("span");
+
+        gsap.fromTo( 
+            spans,
+            {opacity: 0, y: 40},
+            {
+                opacity: 1,
+                y: 0,
+                duration: 0.35,
+                ease: "power3.out",
+                stagger: 0.15,
+            }
+        );
+    }, []);
+
     return (
         <section id="hero">
-            <p>Meet Alexis...</p>
-            <h1>FRONT-END <br/> WEB DEVELOPER</h1>
+            <p>
+                <span>Meet</span>{" "}
+                <span>Alexis...</span>
+            </p>
+
+            <h1 ref={titleRef}>
+                <span>FRONT</span>{" "}
+                <span>END</span><br/>
+                <span>WEB</span>{" "}
+                <span>DEVELOPER.</span>
+            </h1>
 
             <div className="arrow">
-                {Array.from({length: 8}).map((_, i) => (
-                    <div key={i} className="bar"/>
+                {Array.from({ length: 8 }).map((_, i) => (
+                    <div key={i} className="bar" />
                 ))}
             </div>
         </section>
     );
 }
-
-export default Hero;
