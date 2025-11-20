523a3abf22f54b9a6ac944e541fb9e2f9a6192f3
Alexis Luna <mceclipse1889@gmail.com>
2025-11-19 22:05:57 -0600

    Update Priority.jsx

--- src/components/Priority.jsx
+++ src/components/Priority.jsx
@@ -1,7 +1,46 @@
+/* import cdmx from "/mexicoCity.png";
+
 export default function Priority() {
+    
     return (
         <section>
             <h1>Success!</h1>
+            <img className="lazy-reveal" src={cdmx} alt="Mexico City"/>
+        </section>
+    );
+} */
+
+
+/* export default function Priority() {
+    return (
+        <section className="h-screen flex items-center justify-center">
+            <div className="flex flex-col gap-2  text-white font-semibold text-5xl text-center">
+                <h1>I love to make things...</h1>
+                <p>(Design 1 - Akira Bars)</p>
+
+                <h1 className="mt-15 text-9xl font-normal font-[anton] bg-rose-600
+                text-(--bg) w-screen">CONSISTENT</h1>
+
+                <h1 className="text-9xl font-normal font-[anton] bg-rose-700
+                text-(--bg) w-screen">DYNAMIC</h1>
+
+                <h1 className="text-9xl font-normal font-[anton] bg-rose-800
+                text-(--bg) w-screen">BEAUTIFUL</h1>
+
+                <h1 className="text-9xl font-normal font-[anton] bg-rose-900
+                text-(--bg) w-screen">INTUITIVE</h1>
+            </div>
+        </section>
+    );
+} */
+
+export default function Priority() {
+    return (
+        <section className="h-screen bg-sky-950 flex items-center justify-center">
+            <div className="bg-(--bg) w-screen font-semibold p-15">
+                <h1 className="text-white text-5xl ml-25">I love to make things...</h1>
+                <h1 className="text-sky-500 text-9xl ml-25">Consistent.</h1>
+            </div>
         </section>
     );
 }
