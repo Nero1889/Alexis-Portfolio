72f8fe83649887ed9e96220aadc49914d447e58f
Alexis Luna <mceclipse1889@gmail.com>
2025-11-19 22:06:28 -0600

    Add files via upload

---
+++ src/components/Interactive.jsx
@@ -0,0 +1,8 @@
+export default function Interactive() {
+    return (
+        <section id="interactive">
+            <h1 className="text-white font-semibold text-2xl">Interactive
+            Component Success!</h1>
+        </section>
+    )
+}
